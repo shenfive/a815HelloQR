c6d32d0ccd8576ec1d7b22e4b14b3c6dcd761ef3
Danny Shen <shenfive@hotmail.com>
2021-09-26 10:09:39 +0800

    get Cam

--- a815HelloQR/ViewController.swift
+++ a815HelloQR/ViewController.swift
@@ -6,12 +6,27 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var outputVideoView: UIView!
+    @IBOutlet weak var scanTextLabel: UILabel!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let avCaptureSession = AVCaptureSession()
+        guard let avCaptureDevice = AVCaptureDevice.default(for: AVMediaType.video) else {return}
+        guard let avCaptureInput =
+        try? AVCaptureDeviceInput(device: avCaptureDevice) else { return }
+        avCaptureSession.addInput(avCaptureInput) //session 加上輸入
+         
     }
 
 
