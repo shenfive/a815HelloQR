d9f72f83536163171fd69ec5d36029ff13dbe0e0
Danny Shen <shenfive@hotmail.com>
2021-09-26 09:55:05 +0800

    Initial Commit

---
+++ a815HelloQR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  a815HelloQR
+//
+//  Created by 申潤五 on 2021/9/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
